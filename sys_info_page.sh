efd64860d6f9bc56f1cf8375b6e01744ecf83c7a
Ian Culver <iculver@login3.ufhpc>
2020-09-21 23:23:02 -0400

    Added function code

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -7,14 +7,26 @@ TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
 report_uptime () {
 	echo "Function report_uptime executed."
+	cat <<- _EOF_
+		<h2>System Uptime</h2>
+		<pre>$(uptime)</pre>
+		_EOF_
 	return
 }
 report_disk_space () {
 	echo "Function report_disk_space executed."
+	cat <<- _EOF_
+		<h2>Disk Space Utilization</h2>
+		<pre>$(df -h)</pre>
+		_EOF_
 	return
 }
 report_home_space () {
 	echo "Function report_home_space executed."
+	<<- _EOF_
+      		<h2>Home Space Utilization</h2>
+      		<pre>$(du -sh /home/magitz/*)</pre>
+		_EOF_
 	return
 }
 
